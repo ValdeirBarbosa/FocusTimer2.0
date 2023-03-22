a2580c503b02d496cc62d831c04e099cea64bc3b
valdeir.barbosa <valdeir.barbosa@outlook.com>
2023-03-21 23:01:16 -0300

    ultimo ajsute de audio

--- js/sound.js
+++ js/sound.js
@@ -4,7 +4,7 @@ export default function () {
 
   const coffeeShop = new Audio("https://github.com/ValdeirBarbosa/FocusTimer2.0/blob/main/sounds/370973__waweee__coffee-shop-ambience_remasteredmp3-14437.mp3?raw=true")
   const wood = new Audio("https://github.com/ValdeirBarbosa/FocusTimer2.0/blob/main/sounds/amazon-florest-14836.mp3?raw=true")
-  const fire = new Audio("https://github.com/KauaSanttosSouza/sounds-ambient/blob/main/Lareira.wav?raw=true")
+  const fire = new Audio("https://github.com/ValdeirBarbosa/FocusTimer2.0/blob/main/sounds/fireplace-fire-crackling-loop-123930.mp3?raw=true")
   const rain = new Audio("https://github.com/ValdeirBarbosa/FocusTimer2.0/blob/main/sounds/rain.mp3?raw=true")
   const alarm = new Audio("https://github.com/ValdeirBarbosa/FocusTimer2.0/blob/main/sounds/old-mechanic-alarm-clock-140410.mp3?raw=true")
 
